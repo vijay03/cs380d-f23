47071d0e1c09017940e3cda7e798633d88b6d6ae
Shreya Talati <41751613+ShreyaTalati@users.noreply.github.com>
2023-10-05 23:41:03 -0500

    Update run_cluster.py from testKVS

--- project1/run_cluster.py
+++ project1/run_cluster.py
@@ -2,11 +2,14 @@ import argparse
 import os
 import subprocess
 
+import time
 import random
 import xmlrpc.client
-import traceback
-from shared import util
+
 import concurrent.futures
+
+from shared import util
+
 baseAddr = "http://localhost:"
 baseClientPort = 7000
 baseFrontendPort = 8001
@@ -68,22 +71,119 @@ def killServer(k8s_client, k8s_apps_client, serverId):
 
 def shutdownServer(k8s_client, k8s_apps_client, serverId):
     result = frontend.shutdownServer(serverId)
-    remove_node(k8s_client, k8s_apps_client, 'server', serverId)
     print(result)
 
 def put(key, value):
-    result = clientList[random.randint(1, 100000) % len(clientList)].put(key, value)
+    result = clientList[random.choice(list(clientList.keys()))].put(key, value)
     print(result)
 
 def get(key):
-    print('Request for key: {key}')
-    result = clientList[random.randint(1, len(clientList)) % len(clientList)].get(key)
-    print('Request for key later: {key}')
+    result = clientList[random.choice(list(clientList.keys()))].get(key)
     print(result)
 
 def printKVPairs(serverId):
     result = frontend.printKVPairs(serverId)
     print(result)
+
+def loadDataset(thread_id, keys, load_vals, num_threads):
+    start_idx = int((len(keys) / num_threads) * thread_id)
+    end_idx = int(start_idx + (int((len(keys) / num_threads))))
+
+    for idx in range(start_idx, end_idx):
+        result = clientList[thread_id].put(keys[idx], load_vals[idx])
+
+def runWorkload(k8s_client, k8s_apps_client, prefix, thread_id,
+                keys, load_vals, run_vals, num_threads, num_requests,
+                put_ratio, test_consistency, crash_server, add_server, remove_server):
+    request_count = 0
+    start_idx = int((len(keys) / num_threads) * thread_id)
+    end_idx = int(start_idx + (int((len(keys) / num_threads))))
+
+    if test_consistency == 1:
+        while num_requests > request_count:
+            idx = random.randint(start_idx, end_idx - 1)
+            if thread_id == 0 and request_count == int(num_requests / 2):
+                if crash_server == 1:
+                    killServer(k8s_client, k8s_apps_client, 0)
+                elif add_server == 1:
+                    addServer(k8s_client, k8s_apps_client, prefix)
+                elif remove_server == 1:
+                    shutdownServer(k8s_client, k8s_apps_client, 0)
+            newval = random.randint(0, 1000000)
+            clientList[thread_id].put(keys[idx], newval)
+            result = clientList[thread_id].get(keys[idx])
+            result = result.split(':')
+            if int(result[0]) != keys[idx] or int(result[1]) != newval:
+                print("[Error] request = (%d, %d), return = (%d, %d)" % (keys[idx], newval, int(result[0]), int(result[1])))
+                return
+            request_count += 1
+    else:
+        optype = []
+        for i in range(0, 100):
+            if (i % 100) < put_ratio:
+                optype.append("Put")
+            else:
+                optype.append("Get")
+        random.shuffle(optype)
+
+        while num_requests > request_count:
+            for idx in range(start_idx, end_idx):
+                if request_count == num_requests:
+                    break
+                if optype[idx % 100] == "Put":
+                    result = clientList[thread_id].put(keys[idx], run_vals[idx])
+                elif optype[idx % 100] == "Get":
+                    result = clientList[thread_id].get(keys[idx])
+                    result = result.split(':')
+                    if int(result[0]) != keys[idx] or int(result[1]) != load_vals[idx]:
+                        print("[Error] request = (%d, %d), return = (%d, %d)" % (keys[idx], load_vals[idx], int(result[0]), int(result[1])))
+                        return
+                else:
+                    print("[Error] unknown operation type")
+                    return
+                request_count += 1
+
+def testKVS(k8s_client, k8s_apps_client, prefix, num_keys, num_threads,
+            num_requests, put_ratio, test_consistency=0, crash_server=0,
+            add_server=0, remove_server=0):
+    serverList = frontend.listServer()
+    serverList = serverList.split(',')
+    if len(serverList) < 1:
+        print("[Error] Servers do not exist")
+        return
+
+    if len(clientList) < num_threads:
+        print("[Warning] Clients should exist more than # of threads")
+        print("[Warning] Add %d more clients" % (num_threads - len(clientList)))
+        for i in range(len(clientList), num_threads):
+            addClient(k8s_client, k8s_apps_client, prefix)
+
+    keys = list(range(0, num_keys))
+    load_vals = list(range(0, num_keys))
+    run_vals = list(range(num_keys, num_keys * 2))
+
+    random.shuffle(keys);
+    random.shuffle(load_vals);
+    random.shuffle(run_vals);
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_threads)
+    start = time.time()
+    for thread_id in range(0, num_threads):
+        pool.submit(loadDataset, thread_id, keys, load_vals, num_threads)
+    pool.shutdown(wait=True)
+    end = time.time()
+    print("Load throughput = " + str(round(num_keys/(end - start), 1)) + "ops/sec")
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_threads)
+    start = time.time()
+    for thread_id in range(0, num_threads):
+        pool.submit(runWorkload, k8s_client, k8s_apps_client, prefix,
+                    thread_id, keys, load_vals, run_vals,
+                    num_threads, int(num_requests / num_threads), put_ratio,
+                    test_consistency, crash_server, add_server, remove_server)
+    pool.shutdown(wait=True)
+    end = time.time()
+    print("Run throughput = " + str(round(num_requests/(end - start), 1)) + "ops/sec")
 
 def init_cluster(k8s_client, k8s_apps_client, num_client, num_server, ssh_key, prefix):
     global frontend
@@ -106,7 +206,6 @@ def event_trigger(k8s_client, k8s_apps_client, prefix):
     while terminate != True:
         cmd = input("Enter a command: ")
         args = cmd.split(':')
-        print(str(args))
 
         if args[0] == 'addClient':
             addClient(k8s_client, k8s_apps_client, prefix)
@@ -125,32 +224,23 @@ def event_trigger(k8s_client, k8s_apps_client, prefix):
             value = int(args[2])
             put(key, value)
         elif args[0] == 'get':
-            keys = []
-            for i in range(len(args)):
-                if(args[i] == 'get'):
-                    key = int(args[i + 1])
-                    keys.append(key)
-            # key = int(args[1])
-            print(str(keys))
-            try:
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    # Submit GET requests for each key concurrently
-                    print("Inside")
-                    # results = list(executor.map(get, keys))
-                    results = [executor.submit(get, (key)) for key in keys] 
-                    # for future in concurrent.futures.as_completed(results):
-                    #     print(results)
-                    print("Done res")
-                    # for r in results:
-                    #     print(r)
-                concurrent.futures.wait(results)
-            except Exception as e:
-                print("In error")
-                traceback.print_exc()
-            # get(key)
+            key = int(args[1])
+            get(key)
         elif args[0] == 'printKVPairs':
             serverId = int(args[1])
             printKVPairs(serverId)
+        elif args[0] == 'testKVS':
+            num_keys = int(args[1])
+            num_threads = int(args[2])
+            num_requests = int(args[3])
+            put_ratio = int(args[4])
+            test_consistency = int(args[5])
+            crash_server = int(args[6])
+            add_server = int(args[7])
+            remove_server = int(args[8])
+            testKVS(k8s_client, k8s_apps_client, prefix, num_keys, num_threads,
+                    num_requests, put_ratio, test_consistency, crash_server,
+                    add_server, remove_server)
         elif args[0] == 'terminate':
             terminate = True
         else:
